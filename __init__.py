44e3fc4dcc72725008168922b243d92b507b7a6f
resonantsky <discountlinen23@gmail.com>
2026-04-06 10:35:43 +0200

    Remove CFZ_CUDNN and CFZ_CUDNN_Advanced nodes; set cudnn.benchmark=False

--- __init__.py
+++ __init__.py
@@ -1,12 +1,10 @@
-from .cfz_caching_condition import save_conditioning, load_conditioning, CFZ_PrintMarker, CFZ_CUDNN, CFZ_CUDNN_Advanced
+from .cfz_caching_condition import save_conditioning, load_conditioning, CFZ_PrintMarker
 from .cfz_miopen import CFZ_MIOpen_Profile, CFZ_MIOpen_Settings, CFZ_MIOpen_Solvers, CFZ_MIOpen_Paths, CFZ_MIOpen_DBInfo
 
 NODE_CLASS_MAPPINGS = {
     "CFZ_save_conditioning": save_conditioning,
     "CFZ_load_conditioning": load_conditioning,
     "CFZ_PrintMarker": CFZ_PrintMarker,
-    "CFZ_CUDNN": CFZ_CUDNN,
-    "CFZ_CUDNN_Advanced": CFZ_CUDNN_Advanced,
     "CFZ_MIOpen_Profile": CFZ_MIOpen_Profile,
     "CFZ_MIOpen_Settings": CFZ_MIOpen_Settings,
     "CFZ_MIOpen_Solvers": CFZ_MIOpen_Solvers,
@@ -18,8 +16,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "CFZ_save_conditioning": "CFZ Save Conditioning",
     "CFZ_load_conditioning": "CFZ Load Conditioning",
     "CFZ_PrintMarker": "CFZ Print Marker",
-    "CFZ_CUDNN": "CFZ CUDNN",
-    "CFZ_CUDNN_Advanced": "CFZ CUDNN Advanced",
     "CFZ_MIOpen_Profile": "CFZ MIOpen Profile",
     "CFZ_MIOpen_Settings": "CFZ MIOpen Settings",
     "CFZ_MIOpen_Solvers": "CFZ MIOpen Solvers",
